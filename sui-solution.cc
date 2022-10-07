e64fb1750b980834c2cc587816ad2a0b7c24f858
Tereza Burianova <40072706+T-ess@users.noreply.github.com>
2022-10-08 00:06:27 +0200

    Code refactoring

--- sui-solution.cc
+++ sui-solution.cc
@@ -110,7 +110,7 @@ std::vector<SearchAction> DepthFirstSearch::solve(const SearchState &init_state)
 	map<shared_ptr<SearchState>,stateInfo> statesMap;
 	stateInfo info;
 
-	// initial state initialization
+	// initial state
 	shared_ptr<SearchState> init = make_shared<SearchState>(init_state);
 	DFSopen.push_back(init);
 	stateInfo initial = {nullptr, nullptr, 0};
@@ -119,65 +119,47 @@ std::vector<SearchAction> DepthFirstSearch::solve(const SearchState &init_state)
 	if (DFSopen.empty()) return {};
 	shared_ptr<SearchState> actState;
 	while (!DFSopen.empty() && !DFSopen.back()->isFinal()) {
-		if ((getCurrentRSS() + 300000) >= mem_limit_) return {};
-		if (statesMap.count(DFSopen.back()) == 0) return {};
+		if ((getCurrentRSS() + 300000) >= mem_limit_) return {}; // memory check
+		if (statesMap.count(DFSopen.back()) == 0) return {}; // current state is not in statesMap
 		int actDepth = statesMap.at(DFSopen.back()).depth;
-		if (actDepth < depth_limit_) {
+		if (actDepth < depth_limit_) { // depth limit check
 			actState = DFSopen.back();
-			if (DFSclosed.find(*actState) != DFSclosed.end()) {
+			if (DFSclosed.find(*actState) != DFSclosed.end()) { // current state already visited
 				DFSopen.pop_back();
 				continue;
 			}
 			DFSclosed.insert(*actState);
 			DFSopen.pop_back();
-			// expand and push new states
+			// expand current state and push new states
 			vector<SearchAction> currAct = actState->actions();
-			int newStateDepth = 0;
-			if (statesMap.count(actState) == 0) {
-				// invalid
-				cout << "invalid" << endl;
-				return {};
-			} else {
-				newStateDepth = statesMap.at(actState).depth + 1;
-			}
+			int newStateDepth = statesMap.at(actState).depth + 1;
 			for (auto &gameMove : currAct) {
 				SearchState newState = move(gameMove.execute(*actState));
-				// check if newState should be in stack (is not in open or closed)
 				shared_ptr<SearchState> newStatePtr = make_shared<SearchState>(newState);
 				if(DFSclosed.find(*newStatePtr) != DFSclosed.end()) continue; // state found in CLOSED
-
 				DFSopen.push_back(newStatePtr);
 				info = {actState, make_shared<SearchAction>(gameMove), newStateDepth};
 				statesMap.emplace(newStatePtr, info);
 			}
-		} else {
+		} else { // the depth limit has been reached
 			DFSopen.pop_back();
 		}
 	}
-
+	// retrace steps and get actions
 	vector<SearchAction> finalActions;
-	if (DFSopen.empty()) {
-		return {};
-	} else if (DFSopen.back()->isFinal()) {
+	if (!DFSopen.empty() && DFSopen.back()->isFinal()) {
 		// get vector of actions that led to the final state
 		shared_ptr<SearchState> state = DFSopen.back();
-
 		while (statesMap.at(state).action != nullptr) {
-			if (statesMap.find(state) == statesMap.end()) {
-				// invalid
-				return {};
-			} else {
-				// insert the next action to the beginning of the vector
-				finalActions.push_back(*(statesMap.at(state).action));
-				rotate(finalActions.rbegin(), finalActions.rbegin() + 1, finalActions.rend()); // push_front
-				state = statesMap.at(state).parent;
-			}
+			if (statesMap.count(state) == 0) return {};
+			// insert the next action to the beginning of the vector
+			finalActions.push_back(*(statesMap.at(state).action));
+			rotate(finalActions.rbegin(), finalActions.rbegin() + 1, finalActions.rend()); // push_front
+			state = statesMap.at(state).parent; // get the next state
 		}
+		return finalActions;
 	}
-	statesMap.clear();
-	DFSclosed.clear();
-	DFSopen.clear();
-	return finalActions;
+	return {};
 }
 
 double StudentHeuristic::distanceLowerBound(const GameState &state) const {
