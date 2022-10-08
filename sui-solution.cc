b07ae86b1c162f1d8ab40136d1bbe7315377f88b
Tereza Burianova <40072706+T-ess@users.noreply.github.com>
2022-10-09 00:02:26 +0200

    DLS optimalization

--- sui-solution.cc
+++ sui-solution.cc
@@ -7,24 +7,18 @@
 #include "memusage.h"
 using namespace std;
 
-struct stateInfoBFS {
+struct stateInfo {
 	shared_ptr<SearchState> state;
 	vector<shared_ptr<SearchAction>> actions;
-};
-
-struct stateInfoDFS {
-	shared_ptr<SearchState> state;
-	vector<shared_ptr<SearchAction>> actions;
-	int depth;
 };
 
 std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state) {
-// variables
-	deque<stateInfoBFS> BFSopen;
+	// variables
+	deque<stateInfo> BFSopen;
 	set<SearchState> BFSclosed;
 
 	// initial state
-	stateInfoBFS initInfo = {make_shared<SearchState>(init_state), {}};
+	stateInfo initInfo = {make_shared<SearchState>(init_state), {}};
 	BFSopen.push_front(move(initInfo));
 
 	if (BFSopen.empty()) return {};
@@ -57,12 +51,63 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 			finalActions.push_back(*act);
 		}
 		cout << "yay" << endl;
+		printf("BFS: MemLimit %f%%    %lu / %lu\n", (double) getCurrentRSS() / mem_limit_ * 100, getCurrentRSS(), mem_limit_);
 		return finalActions;
 	}
 	return {};
 }
 
 std::vector<SearchAction> DepthFirstSearch::solve(const SearchState &init_state) {
+	// variables
+	deque<stateInfo> DFSopen;
+	set<SearchState> DFSclosed;
+
+	// initial state
+	stateInfo initInfo = {make_shared<SearchState>(init_state), {}};
+	DFSopen.push_back(move(initInfo));
+
+	if (DFSopen.empty()) return {};
+	while (!DFSopen.empty() && !DFSopen.back().state->isFinal()) {
+		if (DFSopen.back().actions.size() < depth_limit_) {
+			shared_ptr<SearchState> actState = move(DFSopen.back().state);
+			vector<shared_ptr<SearchAction>> actStateActions = move(DFSopen.back().actions);
+			if (DFSclosed.find(*actState) != DFSclosed.end()) { // current state already visited
+				DFSopen.pop_back();
+				continue;
+			}
+			DFSclosed.insert(*actState);
+			DFSopen.pop_back();
+			// expand current state and push new states
+			vector<SearchAction> currAct = actState->actions();
+			for (auto &gameMove : currAct) {
+				if ((getCurrentRSS() + 3000000) >= mem_limit_) {
+					cout << "error" << endl;
+					return {}; // memory check
+				}
+				shared_ptr<SearchState> newStatePtr = make_shared<SearchState>(move(gameMove.execute(*actState)));
+				if(DFSclosed.find(*newStatePtr) != DFSclosed.end()) continue; // state found in CLOSED
+				vector<shared_ptr<SearchAction>> actionPath = actStateActions;
+				actionPath.push_back(make_shared<SearchAction>(gameMove));
+				DFSopen.push_back({move(newStatePtr), move(actionPath)});
+			}
+		} else {
+			DFSopen.pop_back();
+		}
+	}
+	vector<SearchAction> finalActions = {};
+	if (!DFSopen.empty() && DFSopen.back().state->isFinal()) {
+		for (auto &act : DFSopen.back().actions) {
+			finalActions.push_back(*act);
+		}
+		cout << "yay" << endl;
+		printf("DFS: MemLimit %f%%    %lu / %lu\n", (double) getCurrentRSS() / mem_limit_ * 100, getCurrentRSS(), mem_limit_);
+		return finalActions;
+	}
+	return {};
+
+
+
+
 	// // variables
 	// deque<shared_ptr<SearchState>> DFSopen;
 	// set<SearchState> DFSclosed;
